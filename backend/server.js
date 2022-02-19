d26fae04ce3fdc4918b1cc5239d70430aaff0583
devenodejs <99832629+devenodejs@users.noreply.github.com>
2022-02-19 16:54:06 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,9 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
+app.get('/api/config/paypal', (req, res) =>{
+   res.send(process.env.PAYPAL_CLINT_ID)
+}
 )
 
 const __dirname = path.resolve()
@@ -55,6 +56,6 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
